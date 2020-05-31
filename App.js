49fa5c5dfbaf9cc5e6d25208d43fbfa74aa3cedd
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-31 14:52:07 +0200

    Imported some libraries

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default class App extends React.Component {
   render() {
