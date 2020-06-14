415a6829eae270e66df3c7ee04cb427ee8262522
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-14 22:03:50 +0200

    Added default func

--- screens/DailyUpdateScreen.js
+++ screens/DailyUpdateScreen.js
@@ -2,3 +2,12 @@ import * as React from 'react'
 import { useState } from 'react'
 import {Button, View, StyleSheet, Text} from 'react-native'
 import {fetchCountriesDailyData} from '../Api'
+export default function DailyUpdateScreen() {
+
+  //Countries()
+
+  return (
+    <Countries/>
+  )
+}
+
