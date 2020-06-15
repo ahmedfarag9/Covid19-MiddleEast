c4e1792070389e0ac3e34bc9a4421429216158a8
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-15 23:38:01 +0200

    Added func to render country data

--- screens/DailyUpdateScreen.js
+++ screens/DailyUpdateScreen.js
@@ -2,6 +2,13 @@ import * as React from 'react'
 import { useState } from 'react'
 import {Button, View, StyleSheet, Text} from 'react-native'
 import {fetchCountriesDailyData} from '../Api'
+function Countries() {
+
+  return (
+  )
+
+}
+
 export default function DailyUpdateScreen() {
 
   //Countries()
