ad0e74a41efdb0b905064738c2fa8bbe6f536ceb
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-15 23:38:35 +0200

    Added state to func component

--- screens/DailyUpdateScreen.js
+++ screens/DailyUpdateScreen.js
@@ -4,6 +4,8 @@ import {Button, View, StyleSheet, Text} from 'react-native'
 import {fetchCountriesDailyData} from '../Api'
 function Countries() {
 
+  const [Country, setCountry] = useState("") 
+
   return (
   )
 
