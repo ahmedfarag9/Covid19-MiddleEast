abdd50069e2296704655c382e9a7fe31077cbd3b
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-16 17:57:00 +0200

    Added style

--- screens/DashboardWorldScreen.js
+++ screens/DashboardWorldScreen.js
@@ -4,3 +4,15 @@ import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
 import { Table, TableWrapper, Row } from 'react-native-table-component';
 import {fetchCountriesDailyData} from '../Api'
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, padding: 16,
+    paddingTop: 30,
+    backgroundColor: 'black' 
+    },
+  header: { height: 50, backgroundColor: '#537791' },
+  text: { textAlign: 'center', fontWeight: '100' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#E7E6E1' }
+});  
+
