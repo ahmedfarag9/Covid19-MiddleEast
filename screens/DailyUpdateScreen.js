51b8663184292f1049767b88073698dc43f58f3c
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-20 02:06:28 +0200

    Added style

--- screens/DailyUpdateScreen.js
+++ screens/DailyUpdateScreen.js
@@ -2,6 +2,21 @@ import * as React from 'react'
 import { useState } from 'react'
 import {Button, View, StyleSheet, Text} from 'react-native'
 import {fetchCountriesDailyData} from '../Api'
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: "space-between",
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: "black",
+      alignItems: 'center',
+      paddingTop: 40,
+      //paddingBottom: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      alignContent : "stretch"
+    },
+  })
+
 function Countries() {
 
   const [Country, setCountry] = useState("") 
