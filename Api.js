8f1d0804f9b621bbe8a0b3a12c9e59b62d6f5a4d
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-23 02:15:28 +0200

    Added new Api url

--- Api.js
+++ Api.js
@@ -2,6 +2,23 @@ export const fetchCountriesDailyData = async () => {
 
   //const movieNameTmp = "'" + movieName + "'"
 
+  const url = "https://coronavirus-19-api.herokuapp.com/countries"
+  
+  const response = await fetch(url, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      //mode: 'cors', // no-cors, *cors, same-origin
+      //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      //credentials: 'same-origin', // include, *same-origin, omit
+      //headers: {
+      //  'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      //},
+      //redirect: 'follow', // manual, *follow, error
+      //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      //body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+
+    const results = await response.json()    
     console.log(results)
     //x = results.Countries.map(processCountries)
     const myCountry = results[25]
