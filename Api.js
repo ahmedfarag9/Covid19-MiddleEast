b6eec4beeba0fcc60e6747d199f926520db567f8
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-23 02:15:10 +0200

    Added func to fetch countries data

--- Api.js
+++ Api.js
@@ -1,3 +1,15 @@
+export const fetchCountriesDailyData = async () => {
+
+  //const movieNameTmp = "'" + movieName + "'"
+
+    console.log(results)
+    //x = results.Countries.map(processCountries)
+    const myCountry = results[25]
+    console.log(results[25])
+
+	return myCountry
+}
+
 
 
 
