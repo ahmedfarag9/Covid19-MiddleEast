da5059bc7506b5e5f9971e761c9f993d1c6de5ba
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-24 02:56:11 +0200

    Added some imports

--- screens/DashboardMiddleEastScreen.js
+++ screens/DashboardMiddleEastScreen.js
@@ -1,1 +1,4 @@
 import * as React from 'react'
+import { useState } from 'react'
+import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
+import { Table, TableWrapper, Row } from 'react-native-table-component';
