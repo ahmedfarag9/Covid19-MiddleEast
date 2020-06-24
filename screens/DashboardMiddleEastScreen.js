1cd656f8470442357d6b40c07afa87702dab8cb0
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-24 02:57:16 +0200

    Added table constants

--- screens/DashboardMiddleEastScreen.js
+++ screens/DashboardMiddleEastScreen.js
@@ -2,3 +2,16 @@ import * as React from 'react'
 import { useState } from 'react'
 import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
 import { Table, TableWrapper, Row } from 'react-native-table-component';
+const tableDataTmp = [];
+/*for (let i = 0; i < 30; i += 1) {
+  const rowData = [];
+  for (let j = 0; j < 13; j += 1) {
+    rowData.push(`${i}${j}`);
+  }
+  tableDataTmp.push(rowData);
+}*/
+
+const middleEastCountries =  [ 'Algeria', 'Bahrain', 'Comoros', 'Djibouti', 'Egypt', 'Iraq', 'Jordan', 'Kuwait',
+                               'Lebanon', 'Libya', 'Mauritania', 'Morocco', 'Oman', 'Palestine', 'Qatar',
+                               'Saudi Arabia', 'Somalia', 'Sudan', 'Syria', 'Tunisia', 'UAE', 'Yemen' ]
+
