1871d93ba88d1b8eb159f07d6b4ef6bb44fa6152
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-29 08:20:14 +0200

    Added style

--- screens/DailyUpdateScreenArabic.js
+++ screens/DailyUpdateScreenArabic.js
@@ -4,3 +4,45 @@ import {Button, View, StyleSheet, Text} from 'react-native'
 import {fetchCountriesDailyData} from '../Api'
 
 const styles = StyleSheet.create({
+    container: {
+      justifyContent: "space-between",
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: "white",
+      alignItems: 'center',
+      paddingTop: 10,
+      paddingBottom: 7,
+      paddingLeft: 20,
+      paddingRight: 20,
+      alignContent : "stretch"
+    },
+    container2:{
+      justifyContent: "center",
+      flex: 0.8,
+      flexDirection: "row",
+      backgroundColor: "white",
+      alignItems: 'center',
+      paddingTop: 5,
+      //paddingBottom: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      alignContent : "stretch",
+      alignItems: "stretch",
+      width: 360
+    
+    },
+    container3:{
+      justifyContent: "space-between",
+      flex: 1.05,
+      flexDirection: "row",
+      backgroundColor: "white",
+      alignItems: 'center',
+      //paddingTop: 10,
+      //paddingBottom: 10,
+      paddingLeft: 30,
+      paddingRight: 30,
+      alignContent : "stretch",
+      alignItems: "stretch",
+      width: 360
+    
+    },
