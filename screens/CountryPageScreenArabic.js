7081c72113a7798533dff1c1ce5f5698f35788de
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-29 08:16:01 +0200

    Added default func

--- screens/CountryPageScreenArabic.js
+++ screens/CountryPageScreenArabic.js
@@ -1,3 +1,12 @@
 import * as React from 'react';
 import {Button, View, StyleSheet, Text} from 'react-native'
 
+
+export default function CountryPageScreenArabic({navigation}) {
+
+    return (
+      <View style={styles.container}>
+
+    </View>
+    )
+  }
