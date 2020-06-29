85220b7b778062f12f4a2b5c6768e09d3490fb6b
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-29 08:21:50 +0200

    Added style

--- screens/DailyUpdateScreenArabic.js
+++ screens/DailyUpdateScreenArabic.js
@@ -46,6 +46,52 @@ const styles = StyleSheet.create({
       width: 360
     
     },
+    container4:{
+      justifyContent: "space-between",
+      flex: 1.05,
+      flexDirection: "row",
+      backgroundColor: "white",
+      alignItems: 'center',
+      //paddingTop: 10,
+      //paddingBottom: 10,
+      paddingLeft: 30,
+      paddingRight: 30,
+      alignContent : "stretch",
+      alignItems: "stretch",
+      width: 360
+    
+    },
+
+    container5:{
+      justifyContent: "space-between",
+      flex: 1.05,
+      flexDirection: "row",
+      backgroundColor: "white",
+      alignItems: 'center',
+      //paddingTop: 10,
+      //paddingBottom: 10,
+      paddingLeft: 30,
+      paddingRight: 30,
+      alignContent : "stretch",
+      alignItems: "stretch",
+      width: 360,
+      //alignSelf: "center"
+    
+    },
+    container6:{
+      justifyContent: "center",
+      flex: 1.05,
+      flexDirection: "row",
+      backgroundColor: "white",
+      alignItems: 'center',
+      //paddingTop: 10,
+      //paddingBottom: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      alignContent : "stretch",
+      alignItems: "stretch",
+      width: 360
+    },
   const middleEastCountries =  [ 'Algeria', 'Bahrain', 'Comoros', 'Djibouti', 'Egypt', 'Iraq', 'Jordan', 'Kuwait',
   'Lebanon', 'Libya', 'Mauritania', 'Morocco', 'Oman', 'Palestine', 'Qatar',
   'Saudi Arabia', 'Somalia', 'Sudan', 'Syria', 'Tunisia', 'UAE', 'Yemen' ]
