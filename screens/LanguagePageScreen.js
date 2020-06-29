fe3161e49ac5efa2dc05b0a1a85f24f80a65018a
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-29 07:57:24 +0200

    Added style

--- screens/LanguagePageScreen.js
+++ screens/LanguagePageScreen.js
@@ -1,3 +1,25 @@
 import * as React from 'react';
 import {Button, View, StyleSheet, Text} from 'react-native'
 
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white",
+    //alignItems: 'center',
+    //paddingTop: 10,
+  },
+  text: {
+    textAlign: 'center',
+    //fontWeight: 'bold',
+    fontSize: 15
+  },
+  text2: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+})  
+  
