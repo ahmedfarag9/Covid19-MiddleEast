8f265d088ccc4c56b030f16868009a23849f54b5
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-29 08:16:25 +0200

    Added style

--- screens/CountryPageScreenArabic.js
+++ screens/CountryPageScreenArabic.js
@@ -2,6 +2,28 @@ import * as React from 'react';
 import {Button, View, StyleSheet, Text} from 'react-native'
 
 
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white",
+    //alignItems: 'center',
+    //paddingTop: 10,
+  },
+  text: {
+    textAlign: 'center',
+    //fontWeight: 'bold',
+    fontSize: 15
+  },
+  text2: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+})  
+  
+
 export default function CountryPageScreenArabic({navigation}) {
 
     return (
