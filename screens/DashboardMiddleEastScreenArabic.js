36faea0c6e635bd169eaeb6f468d8783698fcae0
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-30 13:04:56 +0200

    Added table data

--- screens/DashboardMiddleEastScreenArabic.js
+++ screens/DashboardMiddleEastScreenArabic.js
@@ -8,6 +8,18 @@ const tableHeadConst = ['الدولة', 'اجمالى الاصابات', 'الا
 'المتعافون', 'الحالات النشطة', 'الحالات الحرجة', 'عدد الاصابات / مليون نسمة',
 'عدد الوفيات / مليون نسمة', 'عدد الاختبارات ', 'عدد الاختبارات / مليون نسمة'/*, 'عدد السكان'*/]
 
+const x = 80
+const widthArrConst = [x, x, x, x, x, x, x, x, x, x, x, x/*, x */]
+
+const tableDataTmp = [];
+/*for (let i = 0; i < 30; i += 1) {
+  const rowData = [];
+  for (let j = 0; j < 13; j += 1) {
+    rowData.push(`${i}${j}`);
+  }
+  tableDataTmp.push(rowData);
+}*/
+
 const middleEastCountries =  [ 'Algeria', 'Bahrain', 'Comoros', 'Djibouti', 'Egypt', 'Iraq', 'Jordan', 'Kuwait',
                                'Lebanon', 'Libya', 'Mauritania', 'Morocco', 'Oman', 'Palestine', 'Qatar',
                                'Saudi Arabia', 'Somalia', 'Sudan', 'Syria', 'Tunisia', 'UAE', 'Yemen' ]
