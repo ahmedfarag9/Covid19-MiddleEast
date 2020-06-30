d21f9fa20db903df8e71767676b539d30323ebc2
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-06-30 13:01:44 +0200

    Added table headers data

--- screens/DashboardMiddleEastScreenArabic.js
+++ screens/DashboardMiddleEastScreenArabic.js
@@ -3,3 +3,7 @@ import { useState } from 'react'
 import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
 import { Table, TableWrapper, Row } from 'react-native-table-component';
 import {fetchCountriesDailyData} from '../Api'
+
+const tableHeadConst = ['الدولة', 'اجمالى الاصابات', 'الاصابات الجديدة', 'اجمالى الوفيات', 'الوفيات الجديدة',
+'المتعافون', 'الحالات النشطة', 'الحالات الحرجة', 'عدد الاصابات / مليون نسمة',
+'عدد الوفيات / مليون نسمة', 'عدد الاختبارات ', 'عدد الاختبارات / مليون نسمة'/*, 'عدد السكان'*/]
