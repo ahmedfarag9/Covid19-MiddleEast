deb3e58ac17dfdd39010ebc2694d1ed0576d0d1e
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-01 15:59:25 +0200

    Added table data

--- screens/DashboardWorldScreen.js
+++ screens/DashboardWorldScreen.js
@@ -4,9 +4,22 @@ import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
 import { Table, TableWrapper, Row } from 'react-native-table-component';
 import {fetchCountriesDailyData} from '../Api'
 
+const x = 80
+
 const tableHeadConst = ['Country', 'Total Cases', 'New Cases', 'Total Deaths', 'New Deaths',
 'Total Recovered', 'Active Cases', 'Critical Cases', 'Total Cases / 1M pop',
 'Deaths / 1M pop', 'Total Tests', 'Tests / 1M pop'/*, 'Population'*/]
+
+const widthArrConst = [x, x, x, x, x, x, x, x, x, x, x, x/*, x */]
+
+const tableDataTmp = [];
+/*for (let i = 0; i < 30; i += 1) {
+  const rowData = [];
+  for (let j = 0; j < 13; j += 1) {
+    rowData.push(`${i}${j}`);
+  }
+  tableDataTmp.push(rowData);
+}*/
 
 const styles = StyleSheet.create({
   container: {
