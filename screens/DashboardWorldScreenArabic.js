25fe04e4a547006a312feedcfea465a4626822a6
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-01 16:28:04 +0200

    Added table data

--- screens/DashboardWorldScreenArabic.js
+++ screens/DashboardWorldScreenArabic.js
@@ -7,3 +7,16 @@ import {fetchCountriesDailyData} from '../Api'
 const tableHeadConst = ['الدولة', 'اجمالى الاصابات', 'الاصابات الجديدة', 'اجمالى الوفيات', 'الوفيات الجديدة',
 'المتعافون', 'الحالات النشطة', 'الحالات الحرجة', 'عدد الاصابات / مليون نسمة',
 'عدد الوفيات / مليون نسمة', 'عدد الاختبارات ', 'عدد الاختبارات لكل مليون نسمة'/*, 'عدد السكان'*/]
+
+const x = 80
+const widthArrConst = [x, x, x, x, x, x, x, x, x, x, x, x/*, x */]
+
+const tableDataTmp = [];
+/*for (let i = 0; i < 30; i += 1) {
+  const rowData = [];
+  for (let j = 0; j < 13; j += 1) {
+    rowData.push(`${i}${j}`);
+  }
+  tableDataTmp.push(rowData);
+}*/
+
