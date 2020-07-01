ae6a5f57803da62fea46cdacbb89241c28fd9761
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-01 15:58:30 +0200

    Edited style

--- screens/DashboardWorldScreen.js
+++ screens/DashboardWorldScreen.js
@@ -6,12 +6,14 @@ import {fetchCountriesDailyData} from '../Api'
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, padding: 16,
-    paddingTop: 30,
-    backgroundColor: 'black' 
+    flex: 1,
+    //padding: 0,
+    paddingTop: 0,
+    paddingLeft: 3,
+    backgroundColor: 'white' 
     },
   header: { height: 50, backgroundColor: '#537791' },
-  text: { textAlign: 'center', fontWeight: '100' },
+  text: { textAlign: 'center', fontWeight: '100'},
   dataWrapper: { marginTop: -1 },
   row: { height: 40, backgroundColor: '#E7E6E1' }
 });  
