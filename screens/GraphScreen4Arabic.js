dedf7ba509e77b6364065e7fa995253bcbc50689
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-02 18:30:12 +0200

    Added default func

--- screens/GraphScreen4Arabic.js
+++ screens/GraphScreen4Arabic.js
@@ -1,2 +1,8 @@
 import * as React from 'react';
 import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
+
+export default function GraphsScreen4Arabic()  {
+
+    return (
+    )
+  }
