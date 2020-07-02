da95052e803fa150c9aa6ab3c30358feb5514e38
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-02 18:20:50 +0200

    Added default func

--- screens/GraphScreen1.js
+++ screens/GraphScreen1.js
@@ -1,2 +1,11 @@
 import * as React from 'react';
 import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
+
+  export default function GraphsScreen1() {
+  
+    //Country()
+  
+    return (
+    )
+  }
+  
