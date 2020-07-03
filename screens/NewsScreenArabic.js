2ae79f2b74508a28013fccb3fa676f83d613bc67
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-03 09:36:13 +0200

    Added style

--- screens/NewsScreenArabic.js
+++ screens/NewsScreenArabic.js
@@ -1,2 +1,15 @@
 import * as React from 'react';
 import {Button, View, StyleSheet, Text} from 'react-native'
+
+
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      flex: 1,
+      backgroundColor: "orange"
+    },
+    text: {
+      textAlign: 'center',
+    },
+  })
+  
