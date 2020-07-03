114014c558e826d7782d7db82a6b7642d1f3bb12
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-03 09:38:34 +0200

    Added some imports

--- screens/DashboardCountryScreen.js
+++ screens/DashboardCountryScreen.js
@@ -1,2 +1,5 @@
 import * as React from 'react'
 import { useState } from 'react'
+import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
+import { Table, TableWrapper, Row } from 'react-native-table-component';
+import {fetchCountriesDailyData} from '../Api'
