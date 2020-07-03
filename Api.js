7e8c28d1fb1ccca54b2248ad860af33bf0a19aab
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-03 09:50:51 +0200

    Added some comments

--- Api.js
+++ Api.js
@@ -19,7 +19,7 @@ export const fetchCountriesDailyData = async () => {
     });
 
     const results = await response.json()    
-    console.log(results)
+    //console.log(results)
     //x = results.Countries.map(processCountries)
     //const myCountry = results[25]
     //console.log(results[25])
@@ -29,4 +29,46 @@ export const fetchCountriesDailyData = async () => {
 
 
 
+
+
+
+
+
+
+
+
+/*async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }*/
+
+
+/*
+  --> Other Api's
+  //--> working Api
+  //  const url = "https://api.covid19api.com/summary"
+
+  /// New url --> It only gets total cases, total deaths and total recoverd 
+  const url = "https://corona.lmao.ninja/v2/historical/Egypt"
+
+  /// New url --> Gets all daily data 
+  const url = "'https://coronavirus-19-api.herokuapp.com/countries"
+  const url = "'https://coronavirus-19-api.herokuapp.com/countries/Egypt"  
+  /// Another Api
+  //const url = "https://api.covid19api.com/live/country/egypt/status/confirmed/date/2020-03-21T13:13:30Z"
+
+*/
 
