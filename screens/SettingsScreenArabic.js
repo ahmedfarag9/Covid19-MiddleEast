2a8e28736f343026bed8184b54d085ead6be2214
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-03 09:28:26 +0200

    Adde style

--- screens/SettingsScreenArabic.js
+++ screens/SettingsScreenArabic.js
@@ -1,3 +1,15 @@
 import * as React from 'react';
 import {Button, View, StyleSheet, Text} from 'react-native'
 
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white",
+    //alignItems: 'center',
+    //paddingTop: 10,
+  },
+})  
+
