56b46742a1a35b0abff2c30e376860137dd6cf52
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-06 19:23:48 +0200

    Added default view

--- screens/GraphScreen4Arabic.js
+++ screens/GraphScreen4Arabic.js
@@ -4,5 +4,8 @@ import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
 export default function GraphsScreen4Arabic()  {
 
     return (
+      <View>
+      <Text>Default View</Text>
+    </View>      
     )
   }
