40138e86d43663ac7891c59bcad46403c99be20f
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-06 19:23:06 +0200

    Added default view

--- screens/GraphScreen4.js
+++ screens/GraphScreen4.js
@@ -5,5 +5,8 @@ import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
 export default function GraphsScreen4()  {
 
     return (
+      <View>
+      <Text>Default View</Text>
+    </View>      
     )
   }
