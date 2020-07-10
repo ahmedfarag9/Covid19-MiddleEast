c28d141bde0c82b552e80aab48b9cb34ad2cbc53
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-11 00:33:56 +0200

    Added AdMobBanner options

--- screens/DailyUpdateScreen.js
+++ screens/DailyUpdateScreen.js
@@ -265,6 +265,10 @@ function Countries(props) {
 
       {/* // Display a banner */}
       <AdMobBanner
+        bannerSize="banner"
+        adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+        servePersonalizedAds // true or false
+        //onDidFailToReceiveAdWithError={this.bannerError} 
       />
 
 
