9f6874859de685cf895d8d95f7a0ee0530d4b4df
mustafafarag <mustafafarag23@gmail.com>
2020-07-12 09:38:58 +0200

    Added imports

--- screens/GraphScreen4.js
+++ screens/GraphScreen4.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
+import { useState } from 'react'
+import {fetchCountriesDailyData, fetchCountryID} from '../Api'
 
 
 export default function GraphsScreen4()  {
