e67d5f76961fb29dadafd84e11f7238f031c70e3
mustafafarag <mustafafarag23@gmail.com>
2020-07-15 14:41:55 +0200

    Added some style

--- screens/GraphScreen2.js
+++ screens/GraphScreen2.js
@@ -5,6 +5,15 @@ import {fetchCountriesDailyData, fetchCountryID} from '../Api'
 import { Dimensions } from "react-native";
 import {LineChart, BarChart, PieChart, ProgressChart, ContributionGraph, StackedBarChart} from "react-native-chart-kit"
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, padding: 16,
+        paddingTop: 30, backgroundColor: 'black' 
+    },
+    text: {
+      textAlign: 'center',
+    },
+  })
 
 
 export default function GraphsScreen2()  {
