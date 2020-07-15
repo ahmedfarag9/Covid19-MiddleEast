b52f94cc70bb53746cd8eacf8adf13817dfc0880
mustafafarag <mustafafarag23@gmail.com>
2020-07-15 14:41:31 +0200

    Added some imports

--- screens/GraphScreen2.js
+++ screens/GraphScreen2.js
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import {Button, View, StyleSheet, Text, ScrollView} from 'react-native'
+import { useState } from 'react'
+import {fetchCountriesDailyData, fetchCountryID} from '../Api'
+import { Dimensions } from "react-native";
+import {LineChart, BarChart, PieChart, ProgressChart, ContributionGraph, StackedBarChart} from "react-native-chart-kit"
+
 
 
 export default function GraphsScreen2()  {
