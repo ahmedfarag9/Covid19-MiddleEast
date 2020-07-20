c43aac6e04c2e0f025d9df64cf0920a741c903cd
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-20 13:40:02 +0200

    Edited style

--- screens/DailyUpdateScreenArabic.js
+++ screens/DailyUpdateScreenArabic.js
@@ -13,63 +13,47 @@ import {
 
 
 const styles = StyleSheet.create({
-    container: {
-      justifyContent: "space-between",
-      flex: 1,
-      flexDirection: "column",
-      backgroundColor: "white",
-      alignItems: 'center',
-      paddingTop: 10,
-      paddingBottom: 7,
-      paddingLeft: 20,
-      paddingRight: 20,
-      alignContent : "stretch"
-    },
-    container2:{
-      justifyContent: "center",
-      flex: 0.8,
-      flexDirection: "row",
-      backgroundColor: "white",
-      alignItems: 'center',
-      paddingTop: 5,
-      //paddingBottom: 10,
-      paddingLeft: 20,
-      paddingRight: 20,
-      alignContent : "stretch",
-      alignItems: "stretch",
-      width: 360
-    
-    },
-    container3:{
-      justifyContent: "space-between",
-      flex: 1.05,
-      flexDirection: "row",
-      backgroundColor: "white",
-      alignItems: 'center',
-      //paddingTop: 10,
-      //paddingBottom: 10,
-      paddingLeft: 30,
-      paddingRight: 30,
-      alignContent : "stretch",
-      alignItems: "stretch",
-      width: 360
-    
-    },
-    container4:{
-      justifyContent: "space-between",
-      flex: 1.05,
-      flexDirection: "row",
-      backgroundColor: "white",
-      alignItems: 'center',
-      //paddingTop: 10,
-      //paddingBottom: 10,
-      paddingLeft: 30,
-      paddingRight: 30,
-      alignContent : "stretch",
-      alignItems: "stretch",
-      width: 360
-    
-    },
+  container: {
+    justifyContent: "space-between",
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white",
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    alignContent : "stretch"
+  },
+  container2:{
+    justifyContent: "center",
+    flex: 0.8,
+    flexDirection: "row",
+    backgroundColor: "white",
+    alignItems: 'center',
+    paddingTop: 5,
+    //paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignContent : "stretch",
+    alignItems: "stretch",
+    width: 360
+  
+  },
+  container3:{
+    justifyContent: "space-between",
+    flex: 1.05,
+    flexDirection: "row",
+    backgroundColor: "white",
+    alignItems: 'center',
+    //paddingTop: 10,
+    //paddingBottom: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignContent : "stretch",
+    alignItems: "stretch",
+    width: 360
+  
 
     container5:{
       justifyContent: "space-between",
