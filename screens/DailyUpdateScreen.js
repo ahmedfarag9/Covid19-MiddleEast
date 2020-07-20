ab6ffed2f5acc8275a994566a295b3f01772314e
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-20 13:43:54 +0200

    Added new view component

--- screens/DailyUpdateScreen.js
+++ screens/DailyUpdateScreen.js
@@ -237,12 +237,22 @@ function Countries(props) {
       {/* <Text>{"\n"}</Text> */}
 
       {/* // Display a banner */}
-      <AdMobBanner
-        bannerSize="banner"
-        adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
-        servePersonalizedAds // true or false
+
+
+        <View style={[{paddingLeft: 0}]}>
+
+
+          <AdMobBanner
+            bannerSize="banner"
+            adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+            servePersonalizedAds // true or false
+            //onDidFailToReceiveAdWithError={this.bannerError} 
         //onDidFailToReceiveAdWithError={this.bannerError} 
-      />
+            //onDidFailToReceiveAdWithError={this.bannerError} 
+          />
+
+        </View>
+
 
 
     </View>
