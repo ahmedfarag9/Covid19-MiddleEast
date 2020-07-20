fe5a57d900e4996977aee4a30aa7f2a61780f7b7
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-20 13:40:19 +0200

    Edited style

--- screens/DailyUpdateScreenArabic.js
+++ screens/DailyUpdateScreenArabic.js
@@ -54,70 +54,54 @@ const styles = StyleSheet.create({
     alignItems: "stretch",
     width: 360
   
+  },
+  container4:{
+    justifyContent: "space-between",
+    flex: 1.05,
+    flexDirection: "row",
+    backgroundColor: "white",
+    alignItems: 'center',
+    //paddingTop: 10,
+    //paddingBottom: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignContent : "stretch",
+    alignItems: "stretch",
+    width: 360
+  
+  },
 
-    container5:{
-      justifyContent: "space-between",
-      flex: 1.05,
-      flexDirection: "row",
-      backgroundColor: "white",
-      alignItems: 'center',
-      //paddingTop: 10,
-      //paddingBottom: 10,
-      paddingLeft: 30,
-      paddingRight: 30,
-      alignContent : "stretch",
-      alignItems: "stretch",
-      width: 360,
-      //alignSelf: "center"
-    
-    },
-    container6:{
-      justifyContent: "center",
-      flex: 1.05,
-      flexDirection: "row",
-      backgroundColor: "white",
-      alignItems: 'center',
-      //paddingTop: 10,
-      //paddingBottom: 10,
-      paddingLeft: 20,
-      paddingRight: 20,
-      alignContent : "stretch",
-      alignItems: "stretch",
-      width: 360
-    },
-    text: {
-      textAlign: 'center',
-      paddingTop: 0,
-      fontSize: 40,
-      fontWeight: "bold",
-      color: "black"
-    },
-    text3: {
-      textAlign: 'center',
-      paddingTop: 20,
-      fontSize: 15,
-      //fontWeight: "bold",
-      color: "black"
-    },
-    text2: {
-      justifyContent: "center",
-      textAlign: 'center',
-      //paddingTop: 30,
-      fontSize: 40 ,
-      fontWeight: "bold",
-      color: "black"
-    },
-    circle: {
-      backgroundColor: "white",
-      //alignSelf: "center",
-      height: 100,
-      width: 140,
-      //borderRadius: 95/2,
-      //flex: 2
-      //paddingTop: 10,
-      //paddingBottom: 10,
-    }
-  })
+  container5:{
+    justifyContent: "space-between",
+    flex: 1.05,
+    flexDirection: "row",
+    backgroundColor: "white",
+    alignItems: 'center',
+    //paddingTop: 10,
+    //paddingBottom: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignContent : "stretch",
+    alignItems: "stretch",
+    width: 360,
+    //alignSelf: "center"
+  
+  },
+  container6:{
+    justifyContent: "center",
+    flex: 1.05,
+    flexDirection: "row",
+    backgroundColor: "white",
+    alignItems: 'center',
+    //paddingTop: 10,
+    //paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignContent : "stretch",
+    alignItems: "stretch",
+    width: 360
+  },
+  text: {
 
   const middleEastCountries =  [ 'Algeria', 'Bahrain', 'Comoros', 'Djibouti', 'Egypt', 'Iraq', 'Jordan', 'Kuwait',
   'Lebanon', 'Libya', 'Mauritania', 'Morocco', 'Oman', 'Palestine', 'Qatar',
