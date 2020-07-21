541fe726c5d30e0b10a531034015455a95b279aa
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-21 23:09:54 +0200

    Added new state constants

--- screens/GraphScreen1Arabic.js
+++ screens/GraphScreen1Arabic.js
@@ -38,7 +38,12 @@ function Countries(props){
   const [BackgroundColor, setBackgroundColor] = useState(props.Data.BackgroundColor)
   const [TextColor, setTextColor] = useState(props.Data.TextColor)
   const [todayDate, settodayDate] = useState("1/1/2020")
-      
+  const [TotalCountriesData, setTotalCountriesData] = useState(props.Data.TotalCountriesData)
+  const [MiddleEastCountriesData, setMiddleEastCountriesData] = useState(props.Data.MiddleEastCountriesData)
+  const [Tmp, setTmp] = useState(true)
+
+  const [DailyNewCases, setDailyNewCases] = useState(props.Data.DailyNewCases)
+  const [DailyNewCasesDates, setDailyNewCasesDates] = useState(props.Data.DailyNewCasesDates)
   
       const y = results.map(processTotalCases)
       //console.log(y)
