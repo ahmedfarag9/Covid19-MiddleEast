95f6b9eec54a35255555270c9d96ddd4256572c0
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-21 23:13:11 +0200

    Added new state constants

--- screens/GraphScreen3Arabic.js
+++ screens/GraphScreen3Arabic.js
@@ -25,10 +25,13 @@ import {
     })
   
 
-    const  { TotalDeaths,Dates }   = route.params
 
-    const [Dates1, setDates1] = useState(Dates)
-    const [TotalDeaths1, setTotalDeaths1] = useState(TotalDeaths)
+    const [Country, setCountry] = useState(props.Data.ChossenCountry)
+    const [ChossenCountryData, setChossenCountryData] = useState(props.Data.ChossenCountryData)
+    const [language, setlanguage] = useState(props.Data.language)
+    const [MiddleEastCountries, setMiddleEastCountries] = useState(props.Data.MiddleEastCountries)
+    const [DarkTheme, setDarkTheme] = useState(props.Data.DarkTheme)
+    const [WhiteTheme, setWhiteTheme] = useState(props.Data.WhiteTheme)
 
 
     //console.log(Dates1)
