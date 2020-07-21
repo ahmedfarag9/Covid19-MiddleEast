51db9f83a8bc923f6366d4d224ccf0b1c80a689e
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-21 23:12:09 +0200

    Added new state constants

--- screens/GraphScreen3.js
+++ screens/GraphScreen3.js
@@ -27,6 +27,12 @@ function Countries(props){
   
 
 
+    const [Country, setCountry] = useState(props.Data.ChossenCountry)
+    const [ChossenCountryData, setChossenCountryData] = useState(props.Data.ChossenCountryData)
+    const [language, setlanguage] = useState(props.Data.language)
+    const [MiddleEastCountries, setMiddleEastCountries] = useState(props.Data.MiddleEastCountries)
+    const [DarkTheme, setDarkTheme] = useState(props.Data.DarkTheme)
+    const [WhiteTheme, setWhiteTheme] = useState(props.Data.WhiteTheme)
 
     //console.log(Dates1)
     //console.log(TotalDeaths1)
