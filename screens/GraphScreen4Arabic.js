eb182676dfafab84938e034ff052550b8fe0f55a
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-21 23:15:07 +0200

    Added new state constants

--- screens/GraphScreen4Arabic.js
+++ screens/GraphScreen4Arabic.js
@@ -33,6 +33,11 @@ import {
     const [MiddleEastCountries, setMiddleEastCountries] = useState(props.Data.MiddleEastCountries)
     const [DarkTheme, setDarkTheme] = useState(props.Data.DarkTheme)
     const [WhiteTheme, setWhiteTheme] = useState(props.Data.WhiteTheme)
+    const [CurrentTheme, setCurrentTheme] = useState(props.Data.CurrentTheme)
+    const [BackgroundColor, setBackgroundColor] = useState(props.Data.BackgroundColor)
+    const [TextColor, setTextColor] = useState(props.Data.TextColor)
+    const [todayDate, settodayDate] = useState("1/1/2020")
+    const [TotalCountriesData, setTotalCountriesData] = useState(props.Data.TotalCountriesData)
 
   const processDates = item => (
     item.Date
