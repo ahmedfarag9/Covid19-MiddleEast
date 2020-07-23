f046bab1fc198b168089ce9757c63c2ce134bc09
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-23 14:27:25 +0200

    Added Ad banner

--- screens/GraphScreen3.js
+++ screens/GraphScreen3.js
@@ -115,8 +115,26 @@ function Countries(props){
             }}
           />
         )}
+      </ScrollView>
+
+        <View style={[{paddingLeft: 20}]}>
+
+
+          <AdMobBanner
+            bannerSize="banner"
+            adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+            servePersonalizedAds // true or false
+            //onDidFailToReceiveAdWithError={this.bannerError} 
+          />
+
+        </View>
+
 
       </View>
+
+    )
+  }
+
 
 
   export default function GraphScreen3({route}) {
