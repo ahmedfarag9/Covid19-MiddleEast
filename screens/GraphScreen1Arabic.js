39f73d99aa87ea098a2671c8cd7f2cf7ab483ffa
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-23 14:26:51 +0200

    Added Ad banner

--- screens/GraphScreen1Arabic.js
+++ screens/GraphScreen1Arabic.js
@@ -116,9 +116,23 @@ function Countries(props){
               }}
             />
           )}
+        </ScrollView>
+
+        <View style={[{paddingLeft: 20}]}>
+
+
+          <AdMobBanner
+            bannerSize="banner"
+            adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+            servePersonalizedAds // true or false
+            //onDidFailToReceiveAdWithError={this.bannerError} 
+          />
 
         </View>
-        </ScrollView>
+
+
+        </View>
+
       )
     }
 
