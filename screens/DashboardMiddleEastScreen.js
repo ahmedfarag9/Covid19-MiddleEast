a99e38b36216d6605d8d2f933e4d000032047cd1
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-07-26 15:14:16 +0200

    Commented some code

--- screens/DashboardMiddleEastScreen.js
+++ screens/DashboardMiddleEastScreen.js
@@ -107,12 +107,12 @@ function Countries(props) {
     return (
       <View style={styles.container}>
 
-        <Button
+        {/* <Button
             title="Refresh"
             onPress={() => {
               GetCountries()
             }}
-          />
+          /> */}
 
         <ScrollView horizontal={true}>      
           <View>
